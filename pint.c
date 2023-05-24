75088ac16ed7bdf3f84e712722700376ccea7f32
ZinoChan <devzinochan@gmail.com>
2023-05-24 17:47:15 +0100

    forgot the stderr

--- pint.c
+++ pint.c
@@ -11,7 +11,7 @@ void _pint(stack_t **stack, unsigned int line_num)
 
 	if (*stack == NULL)
 	{
-		fprintf("L%d: can't pint, stack empty\n", line_num);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_num);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
